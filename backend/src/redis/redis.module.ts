43019d31898a54e58f542084fed1c397af6ab3c2
xJam-byte <abylaimuratbayev@gmail.com>
2026-03-24 18:29:36 +0500

    suka ebanaya

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -1,16 +1,28 @@
 import { Module, Global } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+import * as redisStore from 'cache-manager-redis-yet';
 
 @Global()
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
-      store: redisStore as any,
-      host: process.env.REDIS_HOST || 'localhost',
-      port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
-      ttl: 60 * 5, // 5 minutes default
+      useFactory: async () => {
+        if (process.env.REDIS_HOST) {
+          return {
+            store: await redisStore.redisStore({
+              socket: {
+                host: process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+              },
+              ttl: 60 * 5, // 5 minutes default
+            }),
+          };
+        }
+        return {
+          ttl: 60 * 5,
+        };
+      },
     }),
   ],
   exports: [CacheModule],
